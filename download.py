7a55661f58ad5f05330c9a5c29fe188c4e1d6540
Junmin Lee <junminijunmini2004@gmail.com>
2023-01-28 22:56:38 +0900

    Update download.py

--- download.py
+++ download.py
@@ -27,7 +27,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-now_downloading={}
+now_downloading = {}
+
+
 def tdtoko(ti: td):
     ms, s, d = ti.microseconds, ti.seconds, ti.days
 
@@ -46,6 +48,8 @@ def tdtoko(ti: td):
     if ms > 1000:
         return f"{int(ms / 1000)}ms"
     return f"{ms}us"
+
+
 def download(record_time=15, channel_code=24):
     url = f'http://onair.kbs.co.kr/index.html?sname=onair&stype=live&ch_code={channel_code}'
     url2 = f"http://onair.kbs.co.kr/index.html?sname=onair&stype=live&ch_code={channel_code}&ch_type=radioList"
@@ -57,15 +61,17 @@ def download(record_time=15, channel_code=24):
     now = dt.now().strftime("%Y%m%d%H%M%S")
     print(real_url)
     filename = f"/web/music/everymusic_{now}_{record_time}s.mp3"
-    now_downloading[filename] = [dt.now(),False]
-    Thread(target=actual_download, args=(f'ffmpeg -i "{real_url}" -vn -acodec libmp3lame -t {record_time} -metadata title="Every_music_{today_date}" -metadata date="{today_date}" -metadata album="KBS" -metadata track="{today_date}" {filename}', filename)).start()
+    now_downloading[filename] = [dt.now(), False]
+    Thread(target=actual_download, args=(
+    f'ffmpeg -i "{real_url}" -vn -acodec libmp3lame -t {record_time} -metadata title="Every_music_{today_date}" -metadata date="{today_date}" -metadata album="KBS" -metadata track="{today_date}" {filename}',
+    filename)).start()
     return filename
 
-def actual_download(command,filename):
-    process = subprocess.Popen(command,shell=True)
-    process.wait()
-    now_downloading[filename][1]=dt.now()
 
+def actual_download(command, filename):
+    process = subprocess.Popen(command,  stdout=subprocess.PIPE, shell=True)
+    process.wait()
+    now_downloading[filename][1] = dt.now()
 
 
 schedule.every().day.at("18:00").do(download, 7200)
@@ -74,7 +80,9 @@ schedule.every().day.at("18:00").do(download, 7200)
 @app.get("/", response_class=HTMLResponse)
 def index():
     files = os.listdir('/web/music')
-    result = "".join([f"<p>{k} : {tdtoko(dt.now()-v[0])}부터 다운로드 시작, {tdtoko(dt.now()-v[1]) if v[1] else '아직 다운로드 중'}</p>" for k,v in now_downloading.items()])
+    result = "".join([
+                         f"<p>{k} : {tdtoko(dt.now() - v[0])}전부터 다운로드 시작, {f'{tdtoko(dt.now() - v[1])}전에 다운로드 완료' if v[1] else '아직 다운로드 중'}</p>"
+                         for k, v in now_downloading.items()])
     for file in files:
         result += f"<a href='/music/{file}' download='{file}'>{file}</a><audio controls><source src='/music/{file}' type='audio/mp3'></audio><br>"
     if result:
@@ -86,6 +94,8 @@ def index():
 @app.get("/record", response_class=JSONResponse)
 def record(time: int = 15):
     return {"content": download(time)}
+
+
 @app.on_event("startup")
 @repeat_every(seconds=60)
 def every_music() -> None:
