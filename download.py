92032eb04e1b3430e38acc312982c141730f0ec1
Junmin Lee <junminijunmini2004@gmail.com>
2023-01-28 21:47:56 +0900

    Update download.py

--- download.py
+++ download.py
@@ -18,6 +18,6 @@ temp = data[data.find('channel_item') + 35:]
 real_url = temp[:temp.index('"') - 1]
 today_date = dt.now().strftime('%Y%m%d')
 print(real_url)
-subprocess.Popen(f'ffmpeg -i "{real_url}" -vn -acodec libmp3lame -t 15 -metadata title="Every_music_{today_date}" -metadata date="{today_date}" -metadata album="KBS" -metadata track="{today_date}" ./data/everymusic_{today_date}.mp3')
+subprocess.Popen(f'ffmpeg -i "{real_url}" -vn -acodec libmp3lame -t 15 -metadata title="Every_music_{today_date}" -metadata date="{today_date}" -metadata album="KBS" -metadata track="{today_date}" ./data/everymusic_{today_date}.mp3',shell=True)
 
 
